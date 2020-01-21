007e1cda164b1ec830aac74b0e436076f648a9d6
Thiago Batista Barbosa Ribeiro <thi_ago42@hotmail.com>
2020-01-21 19:37:47 -0300

    app1

---
+++ NumeroRandomico/src/estilos/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+
+const folhaEstilos = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#8B10A3'
+    },
+
+    texto1:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+        fontSize: 30,
+        color: '#fff'
+    }
+})
+
+export default folhaEstilos;
+
+
+
---
+++ NumeroRandomico/src/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { View, Button, Text } from "react-native";
+import folhaEstilos from "./estilos/styles";
+
+const numRandomico = ()=>{
+    var num = Math.random();
+    num = Math.floor(num * 10)
+
+    alert(num)
+}
+
+export default class App extends Component {
+    render() {
+        return (
+            <View style = {folhaEstilos.container}>
+                <Text style = {folhaEstilos.texto1}>Olá mundo</Text>
+                <Button title = "Gerar número randomico" 
+                        onPress = {numRandomico}/>
+            </View>
+        )
+    }
+}
