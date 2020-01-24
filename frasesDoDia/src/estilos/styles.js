1b83c2717d3c21e0128e17d13952d0c2680744dd
Thiago Batista Barbosa Ribeiro <thi_ago42@hotmail.com>
2020-01-23 21:09:52 -0300

    app2 and imgs

--- frasesDoDia/src/estilos/styles.js
+++ frasesDoDia/src/estilos/styles.js
@@ -1,1 +1,26 @@
 import { StyleSheet } from "react-native";
+
+
+const Estilos = StyleSheet.create({
+    Principal:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    botao:{
+        backgroundColor: '#538530',
+        paddingVertical: 10,
+        paddingHorizontal: 40,
+        marginTop: 20
+    },
+
+    textoBotao:{
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold'
+    }
+})
+
+
+export default Estilos;
--- frasesDoDia/src/index.js
+++ frasesDoDia/src/index.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image, TouchableOpacity } from "react-native";
+import Estilos from "./estilos/styles";
 
-export default class App extends Component{
-    render(){
-        return(
-            <View>
-                <Text>Frases do dia</Text>
+
+const botaoPressionado = () => {
+
+}
+
+export default class App extends Component {
+    render() {
+        return (
+            <View style={Estilos.Principal}>
+                <Image source={require('./img/logo.png')} />
+                <TouchableOpacity style = {Estilos.botao}>
+                    <Text style = {Estilos.textoBotao}>Nova Frase</Text>
+                </TouchableOpacity>
             </View>
+
         )
     }
 }
